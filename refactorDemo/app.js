567f9b3835734572c21e49b90bae30d85f122916
Tim Wingfield <tim@timwingfield.com>
2014-01-04 19:36:33 -0500

    completed step 3 of the refactoring demo

--- refactorDemo/app.js
+++ refactorDemo/app.js
@@ -1,5 +1,27 @@
 var Country = Backbone.Model.extend({
-  defaults: { distanceFromAbq: "3 parsecs" }
+  parse: function(response, options){
+    var abq = {lat: 35.1107, lng: -106.6100};
+        geoPoint = {lat: response.latlng[0], lng: response.latlng[1]};
+
+    response.distanceFromAbq = this.distHaversine(abq, geoPoint);
+  
+    return response;
+  },
+
+  rad: function(x) { return x*Math.PI/180; },
+
+  distHaversine: function(p1, p2) {
+    var R = 3959, // earth's mean radius in miles
+        dLat  = this.rad(p2.lat - p1.lat),
+        dLong = this.rad(p2.lng - p1.lng),
+
+        a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(this.rad(p1.lat)) * Math.cos(this.rad(p2.lat)) * Math.sin(dLong/2) * Math.sin(dLong/2),
+        c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)),
+        d = R * c;
+
+    return d.toFixed(0);
+  }
+    
 });
 
 var Countries = Backbone.Collection.extend({
